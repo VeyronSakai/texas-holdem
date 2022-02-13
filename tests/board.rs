ca38259dfdfa48ed9f67c922c28c75580f23bcb8
Yuki Sakai <yuukisakai123@gmail.com>
2022-02-14 02:05:24 +0900

    test-macroを使って一部のテストコードを書き換えた

--- tests/board.rs
+++ tests/board.rs
@@ -3,31 +3,61 @@ use texas_holdem::card::Card;
 use texas_holdem::card::State::{self, InDeck, InHand};
 use texas_holdem::suit::Suit;
 
-#[test]
-fn it_works() {
-    #[derive(Debug)]
-    struct TestCase {
-        args: Vec<Card>,
-        expected: Board,
-        name: String,
-    }
+// #[test]
+// fn it_works() {
+//     #[derive(Debug)]
+//     struct TestCase {
+//         args: Vec<Card>,
+//         expected: Board,
+//         name: String,
+//     }
+//
+//     let table = [
+//         TestCase {
+//             args: vec![Card::new(1, Suit::Club, State::InHand)],
+//             expected: Board {
+//                 cards: vec![Card::new(1, Suit::Club, State::InHand)]
+//             },
+//             name: String::from("正常系1"),
+//         },
+//     ];
+//
+//     for test_case in table {
+//         assert_eq!(
+//             Board::new(test_case.args.clone()),
+//             test_case.expected,
+//             "Failed in the {:?}.",
+//             test_case,
+//         );
+//     }
+// }
 
-    let table = [
-        TestCase {
-            args: vec![Card::new(1, Suit::Club, State::InHand)],
-            expected: Board {
-                cards: vec![Card::new(1, Suit::Club, State::InHand)]
-            },
-            name: String::from("正常系1"),
-        },
-    ];
+test_macro::test_eq!(
+    eq_case1,
+    Board::new(vec![Card::new(1, Suit::Club, State::InHand)]) =>
+    Board {cards: vec![Card::new(1, Suit::Club, State::InHand)]}
+);
 
-    for test_case in table {
-        assert_eq!(
-            Board::new(test_case.args.clone()),
-            test_case.expected,
-            "Failed in the {:?}.",
-            test_case,
-        );
-    }
-}
+test_macro::test_eq!(
+    eq_case2,
+    Board::new(vec![Card::new(10, Suit::Heart, State::OnBoard)]) =>
+    Board {cards: vec![Card::new(10, Suit::Heart, State::OnBoard)]}
+);
+
+test_macro::test_ne!(
+    ne_case1,
+    Board::new(vec![Card::new(1, Suit::Club, State::InHand)]) =>
+    Board {cards: vec![Card::new(2, Suit::Club, State::InHand)]}
+);
+
+test_macro::test_ne!(
+    ne_case2,
+    Board::new(vec![Card::new(1, Suit::Club, State::InHand)]) =>
+    Board {cards: vec![Card::new(1, Suit::Heart, State::InHand)]}
+);
+
+test_macro::test_ne!(
+    ne_case3,
+    Board::new(vec![Card::new(1, Suit::Club, State::InHand)]) =>
+    Board {cards: vec![Card::new(1, Suit::Club, State::OnBoard)]}
+);
